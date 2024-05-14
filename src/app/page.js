3682ce15c664574ec156504793a028859caf4071
NicolasDekker <108103073+NicolasDekker@users.noreply.github.com>
2024-05-14 11:32:30 -0300

    primera version mapa y tickers

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Modelos from '../components/Modelos';
+import Marcas from '../components/Marcas';
 import Map from '../components/Map';
 import Footer from '../components/Footer';
 
@@ -6,6 +8,8 @@ export default function Home() {
   return (
     <main>
         <titulo>ASD</titulo>
+          <Marcas />
+          <Modelos />
           <Map />
           <Footer />
     </main>
