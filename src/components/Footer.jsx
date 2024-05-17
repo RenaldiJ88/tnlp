7ecaf791dbb272ceb31b85850087857c0c4cd7e1
migue-ush <albornozmiguel@gmail.com>
2024-05-16 21:32:06 -0300

    Agrego datos de contactos e iconos.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import Imagen from 'next/image';
+import Image from 'next/image';
 import Link from "next/link"
-import { FaInstagram } from "react-icons/fa";
-import { LuFacebook } from "react-icons/lu";
+import { FaInstagram, FaFacebookSquare, FaWhatsapp } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { PiMapPinPlusFill } from "react-icons/pi";
+
 const Footer = () => {
     return (
         <>
-            <div id="container" className='bg-black h-30'>
-                <h1 className='text-white grid place-items-center'>Este es el Footer</h1>
-                <div className='grid place-items-center'>
+            <div id="container" className='bg-black-tnlp h-30 text-white'>
+                <div className='flex place-between'>
+                    <div id="logo_footer" className="flex my-auto">
+                        <Link href="#home">
+                            <Image
+                                src="/img/TN_Logo_PNG.png"
+                                width={175}
+                                height={175}
+                                alt="Logo-Footer"
+                            />
+                        </Link>
+                    </div>
                     <div id="social_media_i" className="flex flex-row my-10 pr-3 md:pr-10 ">
                         <div id="i_facebook">
                             <Link target='_blank' rel="noopener noreferrer" href={'#'}>
-                                <LuFacebook className="text-xl mr-3 text-white hover:text-blue-500" />
+                                <FaFacebookSquare className="text-xl mr-3 hover:text-blue-500" />
+                            </Link>
+                        </div>
+                        <div id="i_whatsapp">
+                            <Link target='_blank' rel="noopener noreferrer" href={'#'}>
+                                <FaWhatsapp className="text-xl mr-3 hover:text-green-500" />
                             </Link>
                         </div>
                         <div id="i_instagram">
                             <Link target='_blank' rel="noopener noreferrer" href={'#'}>
-                                <FaInstagram className="text-xl mr-3 text-white hover:text-red-500" />
+                                <FaInstagram className="text-xl mr-3 hover:text-red-500" />
                             </Link>
+                        </div>
+                        <div>
+                            <div className="flex items-center">
+                                <PiMapPinPlusFill className="mr-2 text-blue-500" />
+                                <p>Bueno Aires, La Plata,  calle 56 Nº1621</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaWhatsapp className="mr-2 text-blue-500" />
+                                <p>221 557 9087</p>
+                            </div>
+                            <div className="flex items-center">
+                                <IoMdMail className="mr-2 text-blue-500" />
+                                <p>tunotebook@gmail.com</p>
+                            </div>
                         </div>
                     </div>
                 </div>
