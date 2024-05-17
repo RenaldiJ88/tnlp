c8bb109c2a17e97775e0245c84f120edcc0c1ae8
migue-ush <albornozmiguel@gmail.com>
2024-05-16 21:29:38 -0300

    Creo el componente de QuienesSomos.

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Modelos from '../components/Modelos';
 import Marcas from '../components/Marcas';
+import QuienesSomos from '../components/QuienesSomos';
 import Map from '../components/Map';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <main>
-        <titulo>ASD</titulo>
+        <title>ASD</title>
           <Marcas />
           <Modelos />
+          <QuienesSomos />
           <Map />
           <Footer />
     </main>
