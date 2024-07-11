98422410e6db3657fe97a781a295004509f41e53
migue-ush <albornozmiguel@gmail.com>
2024-07-11 17:36:00 -0300

    Modificación estilo botón 'Saber Más' del componente QuienesSomos y arreglos en Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     return (
         <>
             <div id="container" className='bg-black-tnlp h-30 text-white'>
-                <div className='flex place-between'>
-                    <div id="logo_footer" className="flex my-auto">
+                <div className='flex justify-between items-center'>
+                    <div id="logo_footer" className="mr-4">
                         <Link href="#home">
                             <Image
                                 src="/img/TN_Logo_PNG.png"
@@ -20,36 +20,35 @@ const Footer = () => {
                             />
                         </Link>
                     </div>
-                    <div id="social_media_i" className="flex flex-row my-10 pr-3 md:pr-10 ">
+                    <div id="social_media_i" className="mx-auto my-10 pr-3 md:pr-10 ">
+                        <div className="flex items-center">
+                            <PiMapPinPlusFill className="mr-2 text-blue-500" />
+                            <p>Bueno Aires, La Plata,  calle 56 Nº1621</p>
+                        </div>
+                        <div className="flex items-center">
+                            <FaWhatsapp className="mr-2 text-blue-500" />
+                            <p>221 557 9087</p>
+                        </div>
+                        <div className="flex items-center">
+                            <IoMdMail className="mr-2 text-blue-500" />
+                            <p>tunotebook@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className='flex mr-4'>
                         <div id="i_facebook">
                             <Link target='_blank' rel="noopener noreferrer" href={'#'}>
-                                <FaFacebookSquare className="text-xl mr-3 hover:text-blue-500" />
+                                <FaFacebookSquare className="text-3xl mr-3 hover:text-blue-500" />
                             </Link>
                         </div>
                         <div id="i_whatsapp">
                             <Link target='_blank' rel="noopener noreferrer" href={'#'}>
-                                <FaWhatsapp className="text-xl mr-3 hover:text-green-500" />
+                                <FaWhatsapp className="text-3xl mr-3 hover:text-green-500" />
                             </Link>
                         </div>
-                        
                         <div id="i_instagram">
                             <Link target='_blank' rel="noopener noreferrer" href={'#'}>
-                                <FaInstagram className="text-xl mr-3 hover:text-red-500" />
+                                <FaInstagram className="text-3xl mr-3 hover:text-red-500" />
                             </Link>
-                        </div>
-                        <div>
-                            <div className="flex items-center">
-                                <PiMapPinPlusFill className="mr-2 text-blue-500" />
-                                <p>Bueno Aires, La Plata,  calle 56 Nº1621</p>
-                            </div>
-                            <div className="flex items-center">
-                                <FaWhatsapp className="mr-2 text-blue-500" />
-                                <p>221 557 9087</p>
-                            </div>
-                            <div className="flex items-center">
-                                <IoMdMail className="mr-2 text-blue-500" />
-                                <p>tunotebook@gmail.com</p>
-                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/QuienesSomos.jsx
+++ src/components/QuienesSomos.jsx
@@ -8,9 +8,9 @@ const QuienesSomos = () => {
         <>
             <div className="bg-black-tnlp text-white flex">
                 <div className="w-1/2 flex flex-col justify-center">
-                    <h2 className="text-3xl text-center mb-3">¿Quiénes somos?</h2>
-                    <p className="text-pretty w-2/5 mx-auto">Somos una empresa con más de 5 años en el mercado. Importando de EEUU los mejores equipos para nuestros clientes. Donde consiguen elevar sus skills y poder ser más competitivos a nivel profesional.</p>
-                    <button className="text-white bg-pink-500 rounded-md h-6 w-60 text-center mt-3 mx-auto">Saber Más</button>
+                    <h2 className="font-exo font-bold text-3xl text-center mb-3">¿Quiénes somos?</h2>
+                    <p className="font-exo text-pretty w-2/5 mx-auto">Somos una empresa con más de 5 años en el mercado. Importando de EEUU los mejores equipos para nuestros clientes. Donde consiguen elevar sus skills y poder ser más competitivos a nivel profesional.</p>
+                    <button className="bg-[#ffffff] opacity-20 py-2 px-4 rounded-xl text-black font-bold border-[#dd40d5] border-2 border-solid w-80 text-center mt-3 mx-auto  hover:text-white hover:bg-[#dd40d5] hover:border-[#ff96fa]">Saber Más</button>
                 </div>
                 <div className="w-1/2 flex justify-center items-center">
                     <Image 
