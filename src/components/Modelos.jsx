8be2ffc7d65664663f78f63182f299051976e264
RenaldiJ88 <jeros_lp@hotmail.com>
2024-11-06 14:58:52 -0300

    arreglando imagenes

--- src/components/Modelos.jsx
+++ src/components/Modelos.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import Image from "next/image";
 import Ticker from 'framer-motion-ticker';
 
-const image1 = '/img/tickers/Modelos/alienware-ticker-2.png';
-const image2 = '/img/tickers/Modelos/rog-strix-ticker-2.png';
-const image3 = '/img/tickers/Modelos/nitro-ticker.png';
-const image4 = '/img/tickers/Modelos/omen-ticker-3.png';
-const image5 = '/img/tickers/Modelos/ideapad-ticker.png';
-const image6 = '/img/tickers/Modelos/katana-ticker.png';
-const image7 = '/img/tickers/Modelos/tuf-ticker.png';
+const image1 = '/img/tickers/modelos/alienware-ticker-2.png';
+const image2 = '/img/tickers/modelos/rog-strix-ticker-2.png';
+const image3 = '/img/tickers/modelos/nitro-ticker.png';
+const image4 = '/img/tickers/modelos/omen-ticker-3.png';
+const image5 = '/img/tickers/modelos/ideapad-ticker.png';
+const image6 = '/img/tickers/modelos/katana-ticker.png';
+const image7 = '/img/tickers/modelos/tuf-ticker.png';
 
 function Modelos() {
     const images = [ image1, image2, image3, image4, image5, image6, image7 ];
