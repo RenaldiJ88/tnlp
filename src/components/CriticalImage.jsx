e16d7782993374660b8dd5a873be857b05ff377c
RenaldiJ88 <jeros_lp@hotmail.com>
2025-08-19 16:28:11 -0300

    Optimizando proyecto + imagenes con Sharp v0.12

--- src/components/CriticalImage.jsx
+++ src/components/CriticalImage.jsx
@@ -15,7 +15,7 @@ const CriticalImage = ({ src, alt, width, height, className = "", sizes, ...prop
       className={className}
       sizes={sizes}
       priority={true}
-      quality={35}
+      quality={65}
       placeholder="empty"
       loading="eager"
       fetchPriority="high"
